ce1f10819d385c50044df389f2f5d2ed8a06ac59
Sophie Au <SophieAu@users.noreply.github.com>
2018-11-19 18:57:41 +0100

    Sophie | Add sidebar-wrapper to make flexbox code less ugly

--- spicy-site/src/Sidebar/Sidebar.js
+++ spicy-site/src/Sidebar/Sidebar.js
@@ -3,18 +3,20 @@ import { Intro, BlogCallToAction, Logo, FooterText } from '../miniFragments.js'
 import './Sidebar.scss';
 
 const Sidebar = () => (
-  <header className="sidebar">
-  <div className="sidebar-header">
-    <div className="sidebar-logo">
-      <Logo />
-    </div>
-    <div className="sidebar-text">
-      <Intro />
-      <BlogCallToAction />
-    </div>
-    </div>
-    <div className="sidebar-footer">
-      <FooterText />
+  <header className="sidebar-wrapper">
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <div className="sidebar-logo">
+          <Logo />
+        </div>
+        <div className="sidebar-text">
+          <Intro />
+          <BlogCallToAction />
+        </div>
+      </div>
+      <div className="sidebar-footer">
+        <FooterText />
+      </div>
     </div>
   </header>
 );
