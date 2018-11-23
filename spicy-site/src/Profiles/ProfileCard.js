dcef3aec7b345b6ea5421f6cd18a3fdc18ef5938
Sophie Au <SophieAu@users.noreply.github.com>
2018-11-23 16:59:09 +0100

    Sophie | Add extended bio point for wide cards

--- spicy-site/src/Profiles/ProfileCard.js
+++ spicy-site/src/Profiles/ProfileCard.js
@@ -5,7 +5,7 @@ import './ProfileCard.scss';
 export default class ProfileCard extends React.Component {
   getSocialMedia = () => Object.entries(this.props.info.socialMedia).map((entry,i) => <a key={i} href={entry[1]}><img src={"/img/social-media/" + entry[0] + ".svg"} alt={"logo of " + entry[0]} /></a>)
 
-  render = ({name, image, city, bio, socialMedia} = this.props.info) => (
+  render = ({name, image, city, bio, bioExt, socialMedia} = this.props.info) => (
     <div className="card">
       <div className="image-side">
         <img src={process.env.PUBLIC_URL + "/img/profiles/" + image + "_tall.jpg"} alt={name}/>
@@ -17,7 +17,7 @@ export default class ProfileCard extends React.Component {
         <div className="info">
           <h4>{city}</h4>
           <h2>{name}</h2>
-          <p>{bio}</p>
+          <p>{bio}<span className="extended-bio"> {bioExt}</span></p>
         </div>
         <div className="social-media">
           <hr />
