fc70d6a6ac642736843edd8707b3dc9e8603b16b
Sophie Au <SophieAu@users.noreply.github.com>
2019-12-31 17:07:45 +0100

    Update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,12 @@
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsx-a11y'],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'jsx-a11y'],
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
     'plugin:jsx-a11y/recommended',
-  ],
-  settings: {
-    react: {
-      version: 'detect', // Tell eslint-plugin-react to autodetect React version
-    },
-  },
-  rules: {
-    'react/prop-types': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-  },
-}
-
-/*
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-hooks', 'simple-import-sort', 'json'],
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable rules from @typescript-eslint/eslint-plugin that conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Keep this line last in the array.
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018,
@@ -40,24 +18,7 @@ module.exports = {
     },
   },
   rules: {
-    'react/jsx-sort-props': [
-      'error',
-      {
-        callbacksLast: true,
-        ignoreCase: true,
-        reservedFirst: ['key', 'ref'],
-      },
-    ],
-    'react/prop-types': false,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'simple-import-sort/sort': 'error',
-    '@typescript-eslint/explicit-function-return-type': false,
-    '@typescript-eslint/explicit-member-accessibility': false,
-    '@typescript-eslint/no-empty-function': false,
-    '@typescript-eslint/no-non-null-assertion': false,
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-use-before-define': false,
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
   },
-}
-*/
+};
