96a33e8236a3ba6e3de5d41560937ff9bcf874ae
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-18 21:45:18 +0100

    Use Gatsby Image for logos

--- src/components/blog/Header.tsx
+++ src/components/blog/Header.tsx
@@ -1,20 +1,41 @@
 import './Header.scss';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
 
 import { paths } from '../../../data/config';
-import logo from '../../../data/img/logo_96.png';
 import strings from '../../../data/strings';
 
-const Header = () => (
-  <header className="header">
-    <a className="header-logo" href={paths.blog} aria-label={strings.Blog.homeLinkAlt}>
-      <img src={logo} alt={strings.Header.logoAlt} />
-    </a>
-    <nav className="header-nav">
-      <a href={paths.about}>{strings.Blog.aboutLink}</a>
-    </nav>
-  </header>
-);
+const query = graphql`
+  query {
+    file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fixed(height: 96, width: 96) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+const Header = () => {
+  const data = useStaticQuery(query);
+
+  return (
+    <header className="header">
+      <a className="header-logo" href={paths.blog} aria-label={strings.Blog.homeLinkAlt}>
+        <Img
+          className="sidebar-logo"
+          fixed={data.file.childImageSharp.fixed}
+          alt={strings.Header.logoAlt}
+        />
+      </a>
+      <nav className="header-nav">
+        <a href={paths.about}>{strings.Blog.aboutLink}</a>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
