5088053a5754414a7abea2223726cee1e1b08c37
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-18 21:45:18 +0100

    Use Gatsby Image for logos

--- src/components/about/Sidebar.tsx
+++ src/components/about/Sidebar.tsx
@@ -1,25 +1,46 @@
 import './Sidebar.scss';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
 
 import Footer from '../_shared/footerFragment';
 import { BlogCallToAction, Intro } from '../_shared/headerFragments';
-import logo from '../../../data/img/logo_256.png';
 import strings from '../../../data/strings';
 
-const Sidebar = () => (
-  <div className="sidebar-wrapper">
-    <div className="sidebar">
-      <header className="sidebar-body">
-        <img className="sidebar-logo" src={logo} alt={strings.Header.logoAlt} />
-        <div className="sidebar-text">
-          <Intro className="intro" />
-          <BlogCallToAction className="blog-cta" />
-        </div>
-      </header>
-      <Footer className="sidebar-footer" />
+const query = graphql`
+  query {
+    file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fixed(height: 256, width: 256) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+const Sidebar = () => {
+  const data = useStaticQuery(query);
+
+  return (
+    <div className="sidebar-wrapper">
+      <div className="sidebar">
+        <header className="sidebar-body">
+          <Img
+            className="sidebar-logo"
+            fixed={data.file.childImageSharp.fixed}
+            alt={strings.Header.logoAlt}
+          />
+          <div className="sidebar-text">
+            <Intro className="intro" />
+            <BlogCallToAction className="blog-cta" />
+          </div>
+        </header>
+        <Footer className="sidebar-footer" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
