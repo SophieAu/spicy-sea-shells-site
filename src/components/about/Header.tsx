63d6772aa4830c11a278bdefbe850e417d2eac3d
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-18 21:45:18 +0100

    Use Gatsby Image for logos

--- src/components/about/Header.tsx
+++ src/components/about/Header.tsx
@@ -1,26 +1,39 @@
 import './Header.scss';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
 
 import { BlogCallToAction, Intro, Title } from '../_shared/headerFragments';
-import logo56 from '../../../data/img/logo_56.png';
-import logo144 from '../../../data/img/logo_144.png';
-import logo192 from '../../../data/img/logo_192.png';
-import logo from '../../../data/img/logo_256.png';
 import strings from '../../../data/strings';
 
-const Header: React.FC<{ id?: string }> = ({ id }) => (
-  <header id={id} className="header">
-    <Title className="title" />
-    <Intro className="text intro" />
-    <BlogCallToAction className="text blog-cta" />
-    <picture className="logo">
-      <source media="(min-width: 1024px)" srcSet={logo192} type="image/png" />
-      <source media="(min-width: 768px)" srcSet={logo144} type="image/png" />
-      <source media="(min-width: 0px)" srcSet={logo56} type="image/png" />
-      <img src={logo} alt={strings.Header.logoAlt} />
-    </picture>
-  </header>
-);
+const query = graphql`
+  query {
+    file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fluid(maxHeight: 384) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+const Header: React.FC<{ id?: string }> = ({ id }) => {
+  const data = useStaticQuery(query);
+  return (
+    <header id={id} className="header">
+      <Title className="title" />
+      <Intro className="text intro" />
+      <BlogCallToAction className="text blog-cta" />
+      <Img
+        fluid={data.file.childImageSharp.fluid}
+        className="logo"
+        alt={strings.Header.logoAlt}
+        imgStyle={{ objectFit: 'contain', objectPosition: 'top center' }}
+      />
+    </header>
+  );
+};
 
 export default Header;
