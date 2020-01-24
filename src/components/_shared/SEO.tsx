916691d59f0291067c16e3787109a2ae80f7f26f
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-24 21:18:51 +0100

    Make background texture a webp

--- src/components/_shared/SEO.tsx
+++ src/components/_shared/SEO.tsx
@@ -3,6 +3,9 @@ import Helmet from 'react-helmet';
 
 import { BASE_URL } from '../../../data/config';
 
+const webpSupportDetection =
+  '!function(e){"use strict";function s(s){if(s){var t=e.documentElement;t.classList?t.classList.add("webp"):t.className+=" webp",window.sessionStorage.setItem("webpSupport",!0)}}!function(e){if(window.sessionStorage&&window.sessionStorage.getItem("webpSupport"))s(!0);else{var t=new Image;t.onload=t.onerror=function(){e(2===t.height)},t.src="data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA"}}(s)}(document);';
+
 interface Props {
   title: string;
   description: string;
@@ -61,6 +64,7 @@ const SEO: React.FC<Props> = ({ title, description, slug, children, creator }) =
       },
     ]}
   >
+    <script>{webpSupportDetection}</script>
     {children}
   </Helmet>
 );
