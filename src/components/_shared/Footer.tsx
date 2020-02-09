acd93c4280e420259db6011f95a0408c1338d1bb
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-09 10:29:12 +0100

    Make Footer and FooterFragment one component

--- src/components/_shared/Footer.tsx
+++ src/components/_shared/Footer.tsx
@@ -2,8 +2,19 @@ import './Footer.scss';
 
 import React from 'react';
 
-import FooterFragment from './footerFragment';
+import chili from '../../../data/img/chili.svg';
+import strings from '../../../data/strings';
 
-const Footer = () => <FooterFragment className="footer" />;
+interface Props {
+  className?: string;
+}
+
+const Footer: React.FC<Props> = ({ className = 'footer' }) => (
+  <footer className={className}>
+    {`${strings.Footer.preImage} `}
+    <img src={chili} alt={strings.Footer.imageAlt} />
+    {` ${strings.Footer.postImage}`}
+  </footer>
+);
 
 export default Footer;
