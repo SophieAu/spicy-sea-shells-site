e55ba703f8a18899377586173c0358276eb9d0e7
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-10 22:38:35 +0100

    Move webp detection into Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 
 import SEO from './SEO';
 
+const webpSupportDetection =
+  '!function(e){"use strict";function s(s){if(s){var t=e.documentElement;t.classList?t.classList.add("webp"):t.className+=" webp",window.sessionStorage.setItem("webpSupport",!0)}}!function(e){if(window.sessionStorage&&window.sessionStorage.getItem("webpSupport"))s(!0);else{var t=new Image;t.onload=t.onerror=function(){e(2===t.height)},t.src="data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA"}}(s)}(document);';
+
 interface Props {
   title: string;
   description: string;
@@ -20,6 +23,7 @@ const Layout: React.FC<Props> = props => {
   return (
     <>
       <SEO title={title} description={description} slug={slug} creator={creator}>
+        <script>{webpSupportDetection}</script>
         {additionalHead}
       </SEO>
       <div id={id}>{children}</div>
