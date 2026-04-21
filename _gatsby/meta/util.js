d5083fc973f09f4ec2c54af6f5367a58c562ee21
Sophie Au <SophieAu@users.noreply.github.com>
2026-04-21 20:46:29 +0200

    remove leftovers

--- _gatsby/meta/util.js
+++
@@ -1,9 +0,0 @@
-exports.createSlug = (rawTitle, date) => {
-  const title = rawTitle
-    .toLowerCase()
-    .replace(/\s/g, '-')
-    .replace(/[^\w-]/g, '')
-    .replace(/(-+)/g, '-');
-
-  return `${date}-${title}`;
-};
