521a52511794936359f8eb5e19a2ea84bd2c54cc
Sophie Au <SophieAu@users.noreply.github.com>
2026-07-02 14:44:34 +0200

    Homepage redirect cleanup since it's static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { paths } from './src/config';
 
 export default defineConfig({
   site: 'https://spicyseashells.com',
+  redirects: { '/': paths.blog },
   integrations: [mdx(), sitemap()],
 });
